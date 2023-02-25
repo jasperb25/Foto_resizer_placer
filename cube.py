ace4bc6d96051c6f864faf8dad4869d36c7f06ff
jasper <15276510+jasperb25@users.noreply.github.com>
2023-02-25 21:32:02 +0100

    Update cube.py
    0.1

--- cube.py
+++ cube.py
@@ -2,9 +2,6 @@ import streamlit as st
 from PIL import Image
 import PyPDF2
 
-uploaded_files = st.file_uploader("Upload 6 images", accept_multiple_files=True, type=['png', 'jpg'])
-target_size = st.slider("Target size in cm", 1, 10, 5)
-
 def resize_image(image, target_size):
     # Convert cm to inches
     target_size_inches = target_size / 2.54
@@ -13,31 +10,39 @@ def resize_image(image, target_size):
     # Resize the image
     return image.resize(new_size)
 
-pdf = PyPDF2.PdfFileWriter()
-page = PyPDF2.pdf.PageObject.createBlankPage(None, 595, 842)
-x, y = 0, 0
+def generate_pdf(uploaded_files, target_size):
+    pdf = PyPDF2.PdfFileWriter()
+    page = PyPDF2.pdf.PageObject.createBlankPage(None, 595, 842)
+    x, y = 0, 0
 
-for file in uploaded_files:
-    image = Image.open(file)
-    resized_image = resize_image(image, target_size)
-    page_left, page_top, page_right, page_bottom = page.mediaBox
-    width, height = resized_image.size
-    if x + width > page_right:
-        y += height
-        x = 0
-    if y + height > page_bottom:
-        pdf.addPage(page)
-        page = PyPDF2.pdf.PageObject.createBlankPage(None, 595, 842)
-        x, y = 0, 0
-    page.mergeTranslatedPage(resized_image.convert("RGB"), x, page_bottom - y - height)
-    x += width
-pdf.addPage(page)
+    for file in uploaded_files:
+        image = Image.open(file)
+        resized_image = resize_image(image, target_size)
+        page_left, page_top, page_right, page_bottom = page.mediaBox
+        width, height = resized_image.size
+        if x + width > page_right:
+            y += height
+            x = 0
+        if y + height > page_bottom:
+            pdf.addPage(page)
+            page = PyPDF2.pdf.PageObject.createBlankPage(None, 595, 842)
+            x, y = 0, 0
+        page.mergeTranslatedPage(resized_image.convert("RGB"), x, page_bottom - y - height)
+        x += width
+    pdf.addPage(page)
 
-output_filename = "output.pdf"
-with open(output_filename, "wb") as out_file:
-    pdf.write(out_file)
+    output_filename = "output.pdf"
+    with open(output_filename, "wb") as out_file:
+        pdf.write(out_file)
 
-pdf = PyPDF2.PdfFileReader(output_filename)
-for i in range(pdf.getNumPages()):
-    page = pdf.getPage(i)
-    st.image(page, use_column_width=True)
+    return output_filename
+
+uploaded_files = st.file_uploader("Upload 6 images", accept_multiple_files=True, type=['png', 'jpg'])
+target_size = st.slider("Target size in cm", 1, 10, 5)
+
+if st.button("Generate PDF"):
+    output_filename = generate_pdf(uploaded_files, target_size)
+    pdf = PyPDF2.PdfFileReader(output_filename)
+    for i in range(pdf.getNumPages()):
+        page = pdf.getPage(i)
+        st.image(page, use_column_width=True)
