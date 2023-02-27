25ee7e401bad1bbf73830b323aa5e2f596a6cfe5
jasper <15276510+jasperb25@users.noreply.github.com>
2023-02-27 09:36:03 +0100

    download button

--- cube.py
+++ cube.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import numpy as np
-import pyvista as pv
-from pyvista import themes
-import io
-from PIL import Image
+import matplotlib.pyplot as plt
+from reportlab.pdfgen import canvas
+from io import BytesIO
 from PyPDF2 import PdfFileWriter, PdfFileReader
+from io import BytesIO
+from streamlit.components.v1 import download_button
 
 
 st.set_page_config(page_title="3D-knipvel", page_icon=":scissors:", layout="wide")
@@ -13,29 +14,45 @@ st.title("3D-knipvel")
 # Slider voor de grootte van de vierkanten in cm
 size_cm = st.slider("Grootte van de vierkanten (cm)", 1, 10, 5, 1)
 
-# Maak een PyVista-blok
-square = pv.Box([0, size_cm, 0, size_cm, 0, size_cm])
+# Maak het matplotlib-blok
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+x, y, z = np.array([[0, 0, 0], [0, size_cm, 0], [size_cm, size_cm, 0], [size_cm, 0, 0], [0, 0, 0]]).T
+ax.plot3D(x, y, z, 'gray')
+ax.plot3D(x+size_cm, y, z, 'gray')
+ax.plot3D(x, y+size_cm, z, 'gray')
+ax.plot3D(x, y, z+size_cm, 'gray')
+ax.plot3D(x, y+size_cm, z+size_cm, 'gray')
+ax.plot3D(x+size_cm, y, z+size_cm, 'gray')
+ax.plot3D(x+size_cm, y+size_cm, z, 'gray')
+ax.plot3D(x+size_cm, y+size_cm, z+size_cm, 'gray')
+ax.set_xlabel('X-as (cm)')
+ax.set_ylabel('Y-as (cm)')
+ax.set_zlabel('Z-as (cm)')
 
-# Maak een PyVista-offscreen-renderer
-plotter = pv.Plotter(off_screen=True)
-plotter.set_background("white")
-plotter.add_mesh(square, color="blue")
-
-# Render het beeld en sla het op als een afbeelding
-image = plotter.screenshot()
-img = Image.fromarray(image)
-
-# Sla de afbeelding op als een PDF
+# Maak een PDF-bestand van het matplotlib-blok
 pdf_output = PdfFileWriter()
-pdf_bytes = io.BytesIO()
-img.save(pdf_bytes, format='PDF')
-pdf_output.addPage(PdfFileReader(io.BytesIO(pdf_bytes.getvalue())).getPage(0))
+pdf_bytes = BytesIO()
+canvas_obj = canvas.Canvas(pdf_bytes)
+fig.savefig(pdf_bytes, format='pdf')
+canvas_obj.showPage()
+canvas_obj.save()
+pdf_output.addPage(PdfFileReader(BytesIO(pdf_bytes.getvalue())).getPage(0))
 pdf_data = pdf_output.getBytes()
 
 # Download de PDF
-b64 = base64.b64encode(pdf_data).decode()
-href = f'<a href="data:application/pdf;base64,{b64}" download="3d_knipvel.pdf">Download de 3D-knipvel als PDF</a>'
-st.markdown(href, unsafe_allow_html=True)
+def download_file():
+    with BytesIO() as data:
+        data.write(pdf_data)
+        data.seek(0)
+        return data.read()
+
+st.download_button(
+    label="Download de 3D-knipvel als PDF",
+    data=download_file(),
+    file_name="PhotoCube.pdf",
+    mime="application/pdf"
+)
 
 # Laat het beeld van de 3D-knipvel zien
-st.image(image, use_column_width=True)
+st.pyplot(fig)
